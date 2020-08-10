26307f4149ae099a2ac28dbb196bd88d9190ec24
malarc <email@email.com>
2020-08-10 14:40:47 -0400

    Remove Unneeded Code

--- server/main.js
+++ server/main.js
@@ -1,32 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import {Tasks} from '/imports/api/tasks';
-
-function insertTask({ title }) {
-  Tasks.insert({ title, createdAt: new Date()});
-  }
-
+import '/imports/api/tasks';
 
 Meteor.startup(() => {
-
   if (!Accounts.findUserByUsername('meteorite')) {
     Accounts.createUser({
       username: 'meteorite',
       password: 'password'
     });
-  }
-
-
-
-
-  if (Tasks.find().count() === 0) {
-    [
-      'First Task',
-      'Second Task',
-      'Third Task',
-      'Fourth Task',
-      'Fifth Task',
-      'Sixth Task',
-      'Seventh Task'
-    ].forEach(insertTask)
   }
 });
