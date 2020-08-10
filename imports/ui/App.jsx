291b03bd8fdb3938dafa6548e199cd95075acf35
malarc <email@email.com>
2020-08-10 11:31:08 -0400

    missing insertTask

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Task } from './Task';
+import Tasks from '/imports/api/tasks';
+import { TaskForm } from './TaskForm';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+const toggleChecked = ({ _id, isChecked }) => {
+  Tasks.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+
+
+
+export const App = () => {
+
+  const tasks = useTracker(() => Tasks.find({}, { sort: { createdAt: -1 } }).fetch());
+
+
+ 
+ 
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+
+ 
+      <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task } onCheckboxClick={toggleChecked} />) }
+      </ul>
+      <TaskForm/>
+    </div>
+  );
+};
